8097b58d623d91f16d2d0fea344dba7f07ff8ab3
Kamil Wójcik <van_kam@wp.pl>
2016-06-23 17:08:43 +0200

    something wrong with images at this point

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -14,6 +14,10 @@ class GameScene: SKScene {
     
     var bg = SKSpriteNode()
     
+    var rura1 = SKSpriteNode()
+    
+    var rura2 = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
@@ -36,32 +40,61 @@ class GameScene: SKScene {
             bg.runAction(przesuńBgNaZawsze)
 
             self.addChild(bg)
-
         }
         
+        var rura1Texture = SKTexture(imageNamed: "rura1.png")
+        var rura1 = SKSpriteNode(texture: rura1Texture)
+        rura1.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame)+300)
+        rura1.setScale(3)
+        
+        self.addChild(rura1)
+        
+        
+        var rura2Texture = SKTexture(imageNamed: "rura2.png")
+        var rura2 = SKSpriteNode(texture: rura2Texture)
+        rura2.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame)-300)
+        rura2.setScale(3)
+
+        self.addChild(rura2)
+        
        
-        var birdTexture1 = SKTexture(imageNamed: "bird1.png") //nie robimy image tylko zamiast jest texture
-        let birdTexture2 = SKTexture(imageNamed: "bird2.png") //dodajemy drugiego ptaszka
-        let birdTexture3 = SKTexture(imageNamed: "bird3.png") //dodajemy drugiego ptaszka
+        let birdTexture1 = SKTexture(imageNamed: "ptak1.png") //nie robimy image tylko zamiast jest texture
+        let birdTexture2 = SKTexture(imageNamed: "ptak2.png") //dodajemy drugiego ptaszka
+        let birdTexture3 = SKTexture(imageNamed: "ptak3.png") //dodajemy drugiego ptaszka
 
         
-        let animacja = SKAction.animateWithTextures([birdTexture1, birdTexture2, birdTexture3], timePerFrame: 0.1) //pozwala nam robić różne rzeczy z węzłami
+        let animacja = SKAction.animateWithTextures([birdTexture1, birdTexture2, birdTexture3], timePerFrame: 0.15) //pozwala nam robić różne rzeczy z węzłami
         let loopAnimacjiPtaszków = SKAction.repeatActionForever(animacja) //loopojemy na zawsze
         
+        
+        
         bird = SKSpriteNode(texture: birdTexture1)
-        bird.setScale(2)
+        bird.setScale(3)
         
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))//musimy powiedziec xcodowi gdzie wsadzić go, cgrectmid daje nam wspolrzedne srodka frame'u czyli naszego telefonu**
         
         bird.runAction(loopAnimacjiPtaszków)//uruchamiamy animacje
         
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture1.size().height/2)
+        bird.physicsBody!.dynamic = true //dodajemy grawitacje
+        
+        
+        
         self.addChild(bird) //tak dodajemy węzeł do widoku
+        
+        var ziemia = SKNode()
+        ziemia.position = CGPointMake(0, 0)
+        ziemia.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ziemia.physicsBody?.dynamic = false //nie chcemy grawitacji 
+        
+        self.addChild(ziemia)
         
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
+        bird.physicsBody!.velocity = CGVectorMake(0, 0)
+        bird.physicsBody?.applyImpulse(CGVectorMake(0, 2))//musimy dodać impulsy
     }
    
     override func update(currentTime: CFTimeInterval) {
